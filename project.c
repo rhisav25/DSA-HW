0797d2182c0a4c58f5e01ad9a1e39f99d55e27e1
rhisav25 <88587556+rhisav25@users.noreply.github.com>
2021-09-30 15:00:35 +0530

    Create project.c

---
+++ project.c
@@ -0,0 +1,73 @@
+/*  
+    Mini-Project 1
+    CS 201 (Data Structures)
+    Department of Computer Science and Engineering, 
+    National Institute of Technology Silchar
+    Rhisav Bora
+    Scholar id- 2012080
+    Time complexity is O(n), with large constant coefficient of size, close to 'length(4185)'.
+*/
+#include<stdio.h>
+#include<time.h>
+
+
+#define MAX 4500
+
+char result[MAX],temp_1[MAX],temp_2[MAX];
+
+void addition(){
+    int carry = 0;
+    for(int i=MAX-1;i>=0;i--){
+        int digit  = (temp_1[i]-'0')+(temp_2[i]-'0')+carry;
+        result[i] = (digit%10) + '0';
+        carry = digit/10;
+    }
+}
+
+
+void fibonacci(int N){
+    if(N==1||N==0){
+        printf("%d",N);
+        return;
+    }
+    for(int i=2;i<=N;i++){
+        addition();
+        for(int i=0;i<MAX;i++){
+            temp_1[i]=temp_2[i];
+            temp_2[i]=result[i];
+            
+        }
+    }
+  int firstDigit = 0;
+  for (int i = 0; i < MAX; i++)
+  {
+    if (firstDigit == 0 && result[i] == '0')
+    {
+      continue;
+    }
+
+    if (firstDigit == 0 && result[i] != '0')
+    {
+      firstDigit = 1;
+    }
+    printf("%c", result[i]);
+  }
+  
+}
+
+
+
+int main(){
+    int N;
+    printf("Enter the fibonacci number to be found :\n");
+    scanf("%d",&N);
+    for(int i=0;i<MAX;i++){
+        temp_1[i]='0';
+        temp_2[i]='0';
+        result[i]='0';
+    }
+    temp_2[MAX-1]='1';
+    printf("The required fibonacci number is : \n");
+    fibonacci(N);
+    printf("\n");
+return 0;}
